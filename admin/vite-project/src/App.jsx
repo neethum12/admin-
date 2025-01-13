9b4a0adecd4f0e9f02dc5967f27125a9bcb3b13e
neethum12 <neethumohan1309@gmail.com>
2025-01-13 10:55:40 +0530

    Update App.jsx

--- admin/vite-project/src/App.jsx
+++ admin/vite-project/src/App.jsx
@@ -9,8 +9,8 @@ import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const url = "https://food-application-mern-r7y7.onrender.com";
 
-  const url= "http://localhost:4000"
 
   
   return (
